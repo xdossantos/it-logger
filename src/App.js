31286c945d13ebeb16cd296994981b96bc994be3
Xola dos Santos <xola.dossantos@xdode.com>
2023-07-28 09:23:34 +0200

    Initial setup (#1)
    * add initial files from project
    
    * add gitignore
    
    * add initial project files for .idea
    
    * add project files

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import { Provider } from 'react-redux'; // used to connect react to redux (app level state)
+import store from './store';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS so we can use modals etc
+    M.AutoInit();
+  });
+
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/actions/techActions.js
@@ -0,0 +1,60 @@
+import {
+  GET_TECHS,
+  ADD_TECH,
+  TECHS_ERROR,
+  SET_LOADING,
+} from './types';
+
+// Get all techs from database
+export const getTechs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/techs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_TECHS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+
+// Add tech to the database
+export const addTech = tech => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/techs', {
+      method: 'POST',
+      body: JSON.stringify(tech),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+
+// Set loading true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
